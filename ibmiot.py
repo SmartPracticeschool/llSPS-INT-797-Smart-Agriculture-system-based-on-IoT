0e2d04da447f2402942c89b985708d8e6d00d54e
infantshiny <shinyaj99@gmail.com>
2020-06-03 17:26:13 +0530

    code commit

---
+++ ibmiot.py
@@ -0,0 +1,51 @@
+import time
+import sys
+import ibmiotf.application # to install pip install ibmiotf
+import ibmiotf.device
+
+#Provide your IBM Watson Device Credentials
+organization = "6eaook" #replace the ORG ID
+deviceType = "smartdevice"#replace the Device type wi
+deviceId = "node1234"#replace Device ID
+authMethod = "use-token-auth"
+authToken = "shiny@1234" #Replace the authtoken
+
+def myCommandCallback(cmd): # function for Callback
+        print("Command received: %s" % cmd.data)
+        if cmd.data['command']=='motor_on':
+                print("motor ON IS RECEIVED")
+                          
+        elif cmd.data['command']=='motor_off':
+                print("motor OFF IS RECEIVED")
+                
+        if cmd.command == "setInterval":
+                
+                if 'interval' not in cmd.data:
+                        print("Error - command is missing required information: 'interval'")
+                else:
+                        interval = cmd.data['interval']
+        elif cmd.command == "print":
+                if 'message' not in cmd.data:
+                        print("Error - command is missing required information: 'message'")
+                else:
+                        output=cmd.data['message']
+                        print(output)
+
+try:
+	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+	deviceCli = ibmiotf.device.Client(deviceOptions)
+	#..............................................
+	
+except Exception as e:
+	print("Caught exception connecting device: %s" % str(e))
+	sys.exit()
+
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
+deviceCli.connect()
+
+while True:
+              
+        deviceCli.commandCallback = myCommandCallback
+
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
